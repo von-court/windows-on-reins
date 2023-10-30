2313e6a29aeeb45beb87ee880c7a527d163b0a67
Matthias <matthias.vongerichten@gmail.com>
2023-10-30 19:44:20 +0100

    simplify power plan selection

--- wor.ps1
+++ wor.ps1
@@ -200,9 +200,8 @@ $disableVBS = 1
 # Disabling it may have a significant performance boost, specially in games
 
 #$powerPlan = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c' # (High performance)
-#$powerPlan = '381b4222-f694-41f0-9685-ff5bb260df2e' # (Balanced)
+$powerPlan = '381b4222-f694-41f0-9685-ff5bb260df2e' # (Balanced)
 #$powerPlan = 'a1841308-3541-4fab-bc81-f71556f20b4a' # (Power saver)
-$powerPlan = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c' # (High performance)
 
 $diskAcTimeout = 0
 $diskDcTimeout = 0
